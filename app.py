fefef764a71b93c823e3586b1fe85159da367055
Alexey Sergeyev <alexey.v.sergeyev@gmail.com>
2025-05-05 22:56:55 +0200

    Fixed bug of Empty ZTF observations

--- app.py
+++ app.py
@@ -513,5 +513,5 @@ def plot_phase():
     })
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=8080, debug=False)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=False)
+    # app.run(debug=True)
